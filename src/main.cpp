aebc6715f154d033b9b85f0b43e5f44d041ea95d
Pablo Caccialupi <pablofac@gmail.com>
2022-06-21 12:26:07 -0300

    New sleeping function added. still to be tested

--- src/main.cpp
+++ src/main.cpp
@@ -244,7 +244,7 @@ void Espera(unsigned int TiempoMillis);
 void Sleep_Forced();
 void readVoltage();
 void printMillis();
-void WakeUpCallBackFunction(void);
+void WakeUpCallBackFunction();
 void ConfigDefault(propAlarm &pa);
 void ConfigWifi();
 void notFound(AsyncWebServerRequest *request);
@@ -577,6 +577,17 @@ void setup() {
   ConfigWifi(); //Wifi initializes after EEPROM reading to have loaded the Alarm Config
   DEBUG_PRINTLN(F("Wifi Conectado"));
 
+  //Configuring the ESP to be able to LIGHT SLEEP:
+  delay(1);                                   //Needs a small delay at the begining!
+  //gpio_pin_wakeup_disable();                //If only timed sleep, not pin interrupt
+  wifi_station_disconnect();                  //disconnect wifi
+  wifi_set_opmode(NULL_MODE);							 	  //set WiFi	mode	to	null	mode.
+  wifi_fpm_set_sleep_type(LIGHT_SLEEP_T);		  //This API can only be called before wifi_fpm_open 	light	sleep
+  wifi_fpm_open();													  //Enable force sleep function
+  for (int i = 0; i < SIZEOF_ZONE; i++)       //If TriggerNC = true, zones must be at ground, opening ground loop wakes the ESP and fires the alarm.
+    wifi_enable_gpio_wakeup(GPIO_ID_PIN(ZONE_PIN[i]), alarmConfig.Zone[i].TriggerNC ? GPIO_PIN_INTR_HILEVEL : GPIO_PIN_INTR_LOLEVEL);
+  wifi_enable_gpio_wakeup(GPIO_ID_PIN(SIM800_RING_RESET_PIN), GPIO_PIN_INTR_LOLEVEL); //Sending this GPIOs to ground will wake the ESP.
+  wifi_fpm_set_wakeup_cb(WakeUpCallBackFunction);	//This API can only be called when force sleep function is enabled, after calling wifi_fpm_open. Will be called after system wakes up only if the force sleep time out (wifi_fpm_do_sleep and the parameter is not 0xFFFFFFF)
 }
 
 void loop() {
@@ -922,56 +933,33 @@ void doAction(){
 }
 
 void Sleep_Forced() {
-  DEBUG_PRINTLN(F("\nForced Light Sleep, wake with GPIO interrupt"));
-
-  wifi_set_sleep_type(LIGHT_SLEEP_T);
-//  for (int i = 0; i < SIZEOF_ZONE; i++)
-//    GPIO_DIS_OUTPUT(GPIO_ID_PIN(ZONE_PIN[i]));    //Configura la pata como entrada
-
-  wifi_station_disconnect();
-  wifi_set_opmode(NULL_MODE);							 	 //	set	WiFi	mode	to	null	mode.
-  wifi_fpm_set_sleep_type(LIGHT_SLEEP_T);		 //This API can only be called before wifi_fpm_open 	light	sleep 
-  wifi_fpm_open();														 //	Enable force sleep function
+  DEBUG_PRINTLN(F("Going to Sleep at ms: ") + String(RTCmillis()));
+  DEBUG_FLUSH;
   
-  //PARA ASIGNAR LA FUNCIÓN ADECUADA A CADA PIN (ESTÁN MULTIPLEXADOS, VER EXCEL)
-  //PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO0_U, FUNC_GPIO0);
-  //PIN_FUNC_SELECT(PERIPHS_IO_MUX_U0TXD_U, FUNC_GPIO1);
-  //PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO2_U, FUNC_GPIO2);
-  //PIN_FUNC_SELECT(PERIPHS_IO_MUX_U0RXD_U, FUNC_GPIO3);
-  //PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO4_U, FUNC_GPIO4);
-  //PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO5_U, FUNC_GPIO5);
-  //PIN_FUNC_SELECT(PERIPHS_IO_MUX_SD_CLK_U, FUNC_GPIO6);
-  //PIN_FUNC_SELECT(PERIPHS_IO_MUX_SD_DATA0_U, FUNC_GPIO7);
-  //PIN_FUNC_SELECT(PERIPHS_IO_MUX_SD_DATA1_U, FUNC_GPIO8);
-  //PIN_FUNC_SELECT(PERIPHS_IO_MUX_SD_DATA2_U, FUNC_GPIO9);
-  //PIN_FUNC_SELECT(PERIPHS_IO_MUX_SD_DATA3_U, FUNC_GPIO10);
-  //PIN_FUNC_SELECT(PERIPHS_IO_MUX_SD_SD_CMD_U, FUNC_GPIO11);
-//  PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTDI_U, FUNC_GPIO12); //Use	MTDI	pin	as	GPIO12.
-//  PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTCK_U, FUNC_GPIO13); //Use	MTCK	pin	as	GPIO13.
-  //PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTMS_U, FUNC_GPIO14); //Use	MTMS	pin	as	GPIO14.
-  //PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTDO_U, FUNC_GPIO15); //Use	MTDO	pin	as	GPIO15.
+  extern os_timer_t *timer_list;  //for timer-based light sleep to work, the os timers need to be disconnected
+  timer_list = nullptr;
 
-  for (int i = 0; i < SIZEOF_ZONE; i++)
-    wifi_enable_gpio_wakeup(GPIO_ID_PIN(ZONE_PIN[i]), GPIO_PIN_INTR_LOLEVEL); //HILEVEL    other option: //wifi_enable_gpio_wakeup(GPIO_ID_PIN(WAKE_UP_PIN), GPIO_PIN_INTR_LOLEVEL);
+  //If forced light sleep without timed wake up, only external gpio wake:
+  //sint8 res = wifi_fpm_do_sleep(0xFFFFFFF);
+  //delay(10);
 
-  wifi_fpm_set_wakeup_cb(WakeUpCallBackFunction);			//This API can only be called when force sleep function is enabled, after calling wifi_fpm_open
-  //fpm_wakeup_cb_func1 will be called after system wakes up only if the force sleep time out (wifi_fpm_do_sleep and the parameter is not 0xFFFFFFF)
-
-  sint8 res = wifi_fpm_do_sleep(0xFFFFFFF);
-  delay(10);
-  DEBUG_PRINTLN(F("Woke up!"));  // the interrupt callback hits before this is executed
-  DEBUG_PRINTLN(F("RESULTADO DEL SLEEP: ") + String(res));
+  //If timed light sleep and external gpio wake:
+  sint8 res = wifi_fpm_do_sleep(SLEEP_TIME_MS * 1000);  //microseconds
+  delay(SLEEP_TIME_MS + 1);  // it goes to sleep //The system will not enter sleep mode instantly when force-sleep APIs are called, but only after executing an idle task.
+  
+  DEBUG_PRINTLN(F("Sleep result (0 is ok): ") + String(res));  // the interrupt callback hits before this is executed
   //0, setting successful;
   //-1, failed to sleep, sleep status error;
   //-2, failed to sleep, force sleep function is not enabled
 }
 
-void WakeUpCallBackFunction(void)
+void WakeUpCallBackFunction()
 {
-  wifi_fpm_close();					 	 //	disable	force	sleep	function
-  //wifi_set_opmode(STATION_MODE);							//	set	station	mode
-  //wifi_station_connect();										 //	connect	to	AP
-  DEBUG_PRINTLN(F("FUNCIÓN CALLBACK EJECUTADA"));
+  DEBUG_PRINTLN(F("Woke Up - CallBack Function executed at ms: ") + String(RTCmillis()));
+  DEBUG_FLUSH;
+  //wifi_fpm_close();					 	        //Only if not sleeping gain, disable force sleep function
+  //wifi_set_opmode(STATION_MODE);			//If need to set station mode
+  //wifi_station_connect();							//If need to connect to AP
 }
 
 uint32_t RTCmillis() {
